3832989d1e12abe022c0f7ebff51a37b4399b3f0
tker <tobyrussell@inspiredsystems.com.au>
2014-05-30 16:48:50 +0800

    only had time to do a little bit today... Committed in case I get bored over long weekend

--- NRedisApi.Fluent.Test/TestFluentRedisOperation.cs
+++ NRedisApi.Fluent.Test/TestFluentRedisOperation.cs
@@ -1,5 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using NUnit.Framework;
+using StackExchange.Redis;
 
 namespace NRedisApi.Fluent.Test
 {
@@ -8,11 +10,30 @@ namespace NRedisApi.Fluent.Test
     {
         private const string TestUrn = "urn:Test";
         const BindingFlags BindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+        private SystemMonitorState _smsToSave;
 
+        private IDatabase _redis;
+        private ConnectionMultiplexer _multiplexer;
+
+        [SetUp]
+        public void SetupRedisConnectionMultiplexer()
+        {
+            _smsToSave = new SystemMonitorState(10, "N107W", DateTime.Now, SystemMonitorStatus.Normal);
+
+            _multiplexer = ConnectionMultiplexer.Connect("localhost");
+
+            _redis = _multiplexer.GetDatabase();
+        }
+
+        [TearDown]
+        public void TeardownRedisConnectionMultiplexer()
+        {
+
+        }
         [Test]
         public void TestRedisOperationConstructor()
         {
-            var redisOperation = new RedisOperation();
+            var redisOperation = new RedisOperation(_redis);
             
             AssertUrnFieldIsEmpty(redisOperation);
 
@@ -22,7 +43,7 @@ namespace NRedisApi.Fluent.Test
         [Test]
         public void TestRedisOperationUrn()
         {
-            var redisOperation = new RedisOperation();
+            var redisOperation = new RedisOperation(_redis);
             redisOperation = redisOperation.Urn(TestUrn);
             AssertUrnFieldEqualsTestUrn(redisOperation);
 
@@ -31,14 +52,28 @@ namespace NRedisApi.Fluent.Test
         [Test]
         public void TestRedisOperationAsType()
         {
-            var redisOperation = new RedisOperation();
+            var redisOperation = new RedisOperation(_redis);
             redisOperation = redisOperation.Urn(TestUrn).String().AsType<string>();
 
             AssertRedisDataStructureFieldIsNotUnknown(redisOperation);
             AssertUrnFieldEqualsTestUrn(redisOperation);
 
-            var type = redisOperation.GetType();
             Assert.IsInstanceOf<RedisOperation<string>>(redisOperation);
+        }
+
+        [Test]
+        public void TestRedisOperationGet()
+        {
+            var redisOperation = new RedisOperation(_redis);
+            var typedRedisOperation = 
+                redisOperation
+                .Urn(TestUrn)
+                .String()
+                .AsType<string>();
+            
+            var returned = typedRedisOperation.Get();
+
+            Assert.IsInstanceOf<RedisOperation<string>>(typedRedisOperation);
         }
 
         private void AssertRedisDataStructureFieldIsUnknown(RedisOperation redisOperation)
--- NRedisApi.Fluent/RedisOperation.cs
+++ NRedisApi.Fluent/RedisOperation.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace NRedisApi.Fluent
 {
@@ -10,9 +8,11 @@ namespace NRedisApi.Fluent
     {
         protected string _urn;
         protected RedisDataStructure _redisDataStructure;
+        private readonly IDatabase _redis;
 
-        public RedisOperation()
+        public RedisOperation(IDatabase redis)
         {
+            _redis = redis;
             _urn = string.Empty;
             _redisDataStructure = RedisDataStructure.Unknown;
         }
@@ -53,18 +53,45 @@ namespace NRedisApi.Fluent
             return this;
         }
 
-        public RedisOperation<T> AsType<T>()
+        public virtual RedisOperation<T> AsType<T>()
         {
-            return new RedisOperation<T>(_urn, _redisDataStructure);
+            return new RedisOperation<T>(_urn, _redisDataStructure, _redis);
         }
     }
 
     public class RedisOperation<T> : RedisOperation
     {
-        public RedisOperation(string urn, RedisDataStructure redisDataStructure)
+        private readonly IDatabase _redis;
+
+        internal RedisOperation(string urn, RedisDataStructure redisDataStructure, IDatabase redis)
+            : base(redis)
         {
+            _redis = redis;
             _urn = urn;
             _redisDataStructure = redisDataStructure;
+        }
+
+        public IEnumerable<T> GetCollection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RedisOperation<U> AsType<U>()
+        {
+            throw new TypeAlreadySpecifiedException("Type already specified. You cannot set two <T> return types");
+        }
+    }
+
+    public class TypeAlreadySpecifiedException : Exception
+    {
+        public TypeAlreadySpecifiedException(string message) : base(message)
+        {
+
         }
     }
 }
