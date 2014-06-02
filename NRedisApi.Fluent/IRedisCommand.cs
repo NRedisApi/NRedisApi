d53f9c4c9769ceadd7dae5f5fb5d26eb65c31057
tker <tobyrussell@inspiredsystems.com.au>
2014-06-03 07:21:00 +0800

    added rediscommand interfaces

---
+++ NRedisApi.Fluent/IRedisCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace NRedisApi.Fluent
+{
+    public interface IRedisStringOperations
+    {
+        void Remove();
+    }
+
+    public interface IRedisCommand
+    {
+        IRedisCommand Expires(long seconds);
+        IRedisCommand Urn(string urn);
+        IRedisCommand<T> As<T>();
+        IStringRedisCommand RedisString();
+    }
+
+    public interface IStringRedisCommand : IRedisCommand, IRedisStringOperations
+    {
+    }
+
+    public interface IRedisCommand<T>
+    {
+        IRedisCommand<T> Expires(long seconds);
+        IRedisCommand<T> Urn(string urn);
+        IStringRedisCommand<T> RedisString();
+        T Get();
+        void Set();
+    }
+
+    public interface IStringRedisCommand<T> : IRedisCommand<T>, IRedisStringOperations
+    {
+    }
+}
